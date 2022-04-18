d595add6d7d9a143ff22191d262c08a9aff50735
Antonio Schettino <schettino@eur.nl>
2022-04-18 09:22:51 +0200

    add .name_repair to as_tibble (to avoid warning)

--- code/functions/split_into_multiple.R
+++ code/functions/split_into_multiple.R
@@ -6,7 +6,7 @@ split_into_multiple <- function(column, pattern = ",", into_prefix) {
   cols <- str_split_fixed(column, pattern, n = Inf)
   # Sub out the ""'s returned by filling the matrix to the right, with NAs which are useful
   cols[which(cols == "")] <- NA
-  cols <- as_tibble(cols)
+  cols <- as_tibble(cols, .name_repair = "unique")
   # name the 'cols' tibble as 'into_prefix_1', 'into_prefix_2', ..., 'into_prefix_m'
   # where m = # columns of 'cols'
   m <- dim(cols)[2]
