57092ba13a65f7f6f659f5348718ae580bb0c0d4
Antonio Schettino <schettino@eur.nl>
2022-04-20 16:45:15 +0200

    cluster 2 complete

--- code/00_preprocessing.R
+++ code/00_preprocessing.R
@@ -349,61 +349,40 @@ write_csv(
   here("data", "preproc", paste0("cluster_", num_cluster, ".csv"))
 )
 
+# Cluster 2 ----------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-num_cluster <- 1
+num_cluster <- 2
 
 cluster <-
-  ERIM_OS_clean %>%
-  filter(
-    Finished == "TRUE" & # keep only complete questionnaires
-      cluster == num_cluster # keep only questions of relevant cluster
-  ) %>%
+  OS_data_clean %>%
+  filter(cluster == num_cluster) %>% # keep only questions of relevant cluster
   droplevels() %>% # drop unused levels
-  select(-c(Finished, cluster)) %>% # drop unused columns
+  select(-cluster) %>% # drop unused column
   select_if(~ sum(!is.na(.)) > 0) %>% # keep columns without NAs
-  rename("item" = "value_1") %>%
-  mutate(question = factor(
-    question,
-    levels = c(
-      "What is your experience with open science practices?",
-      "Are you sharing your knowledge about open science practices with others?",
-      "During March 2021 ERIM launched an ORCID campaign. Did you participate in it and got your own ORCID iD?"
+  rename("item" = "value_1") %>% # rename column (for better readability)
+  mutate(
+    question = factor( # assign order questions
+      question,
+      levels = c(
+        "In your opinion, how important are open data, materials, and/or code for your work?",
+        "What is your experience with using open data, materials, and/or code developed by others?",
+        "What is your experience with openly sharing data, materials, and/or code that you developed?",
+        "Are you familiar with the FAIR principles for data and code?",
+        "The following are possible concerns that researchers could have about making data, materials, and/or code developed by them openly available. Which of these concerns would you agree with?"
+      ),
+      ordered = TRUE
     ),
-    ordered = TRUE
-  )) %>%
+    item = gsub("Other_", "", item) # delete "Other_" from free text responses
+  ) %>%
   group_by(question, item) %>%
-  summarize(number_responses = n()) %>%
+  summarize( # count number of responses per question and item
+    number_responses = n(),
+    .groups = "keep"
+  ) %>%
   ungroup() %>%
   group_by(question) %>%
   mutate(
-    prop = number_responses / sum(number_responses), # proportion
-    perc = round(prop * 100, 2), # percentage
+    perc = round(number_responses / sum(number_responses, na.rm = FALSE) * 100, 2), # percentage
     lab_perc = paste(perc, "%", sep = "") # percentage as text (for labels)
   ) %>%
   ungroup()
@@ -411,10 +390,27 @@ cluster <-
 # save
 write_csv(
   cluster,
-  here("data", "preproc", paste0("cluster", num_cluster, ".csv"))
+  here("data", "preproc", paste0("cluster_", num_cluster, ".csv"))
 )
 
-# Cluster 2 ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 num_cluster <- 2
 
