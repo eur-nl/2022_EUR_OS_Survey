45728b543a5310bc2226bd0ad151383d2bd3a6f8
Antonio Schettino <schettino@eur.nl>
2022-04-20 17:13:10 +0200

    cluster 7 complete

--- code/00_preprocessing.R
+++ code/00_preprocessing.R
@@ -564,49 +564,36 @@ write_csv(
 
 # Cluster 7 ----------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 num_cluster <- 7
 
 cluster <-
-  ERIM_OS_clean %>%
-  filter(
-    Finished == "TRUE" & # keep only complete questionnaires
-      cluster == num_cluster # keep only questions of relevant cluster
-  ) %>%
+  OS_data_clean %>%
+  filter(cluster == num_cluster) %>% # keep only questions of relevant cluster
   droplevels() %>% # drop unused levels
-  select(-c(Finished, cluster)) %>% # drop unused columns
+  select(-cluster) %>% # drop unused column
   select_if(~ sum(!is.na(.)) > 0) %>% # keep columns without NAs
-  rename("item" = "value_1") %>%
-  mutate(question = factor(question)) %>%
+  rename("item" = "value_1") %>% # rename column (for better readability)
+  mutate(
+    question = factor( # assign order questions
+      question,
+      levels = c(
+        "Do you feel recognized and rewarded by EUR (e.g. in the R&O cycle or appraisal conversation) for the Open Science activities you undertake?",
+        "In what way were you recognized and rewarded?",
+        "In what way do you expect to be recognized and rewarded?"
+      ),
+      ordered = TRUE
+    ),
+    item = gsub("Other_", "", item) # delete "Other_" from free text responses
+  ) %>%
   group_by(question, item) %>%
-  summarize(number_responses = n()) %>%
+  summarize( # count number of responses per question and item
+    number_responses = n(),
+    .groups = "keep"
+  ) %>%
   ungroup() %>%
   group_by(question) %>%
   mutate(
-    prop = number_responses / sum(number_responses), # proportion
-    perc = round(prop * 100, 2), # percentage
+    perc = round(number_responses / sum(number_responses, na.rm = FALSE) * 100, 2), # percentage
     lab_perc = paste(perc, "%", sep = "") # percentage as text (for labels)
   ) %>%
   ungroup()
@@ -614,10 +601,33 @@ cluster <-
 # save
 write_csv(
   cluster,
-  here("data", "preproc", paste0("cluster", num_cluster, ".csv"))
+  here("data", "preproc", paste0("cluster_", num_cluster, ".csv"))
 )
 
 # Cluster 8 ----------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 num_cluster <- 8
 
