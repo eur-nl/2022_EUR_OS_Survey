7d442299d23ef82fc09c3c0e9c18f4d1345c5abf
Antonio Schettino <schettino@eur.nl>
2022-06-03 16:55:05 +0200

    complete figure 1

--- code/figure_01.R
+++ code/figure_01.R
@@ -23,17 +23,12 @@ source(here("code", "functions", "theme_custom.R")) # custom ggplot2 theme
 # Data ----------------------------------------------------------------
 
 cluster <-
-  read_csv(
-    here("data", "preproc", "cluster_0.csv"),
-    show_col_types = FALSE
-  ) %>% 
-  mutate(
-    question = as_factor(question),
-    item = as_factor(item)
-  )
-
+  readRDS(here("data", "preprocessed", "rds", "cluster_0.rds")) %>% 
+  filter(question != "Department") %>% 
+  droplevels()
+  
 # extract questions
-questions <- levels(cluster$question)
+questions <- unique(cluster$question)
 
 # Question 1, lollipop graph ----------------------------------------------------------------
 
@@ -41,26 +36,28 @@ num_question <- 1
 
 data_cluster0_question1 <-
   cluster %>%
-  filter(question == questions[num_question]) %>%
-  droplevels() %>%
+  filter(
+    Finished == TRUE & # only completed surveys
+    question == questions[num_question]
+    ) %>%
+  droplevels() %>% 
+  pivot_wider(
+    id_cols = participant,
+    names_from = question,
+    values_from = item
+  ) %>%
+  count(School) %>% 
   mutate(
-    item = recode_factor( # recode item values too long to be displayed on axis
-      item,
-      "Erasmus School of Health Policy & Management (ESHPM)" = "ESHPM",
-      "Erasmus School of History, Culture and Communication (ESHCC)" = "ESHCC",
-      "Erasmus School of Law (ESL)" = "ESL",
-      "Erasmus School of Philosophy (ESPHIL)" = "ESPhil",
-      "Erasmus School of Social and Behavioural Sciences (ESSB)" = "ESSB",
-      "International Institute of Social Studies (ISS)" = "ISS"
-    )
+    perc = round(n / sum(n) * 100, 2),
+    lab_perc = paste(perc, "%", sep = "")
   )
 
 lollipop_cluster0_question1 <-
   data_cluster0_question1 %>%
-  ggplot(aes(x = reorder(item, perc), y = perc)) +
+  ggplot(aes(x = reorder(School, perc), y = perc)) +
   geom_point(size = 6, color = "#0C8066") +
-  geom_segment(aes(x = item, xend = item, y = 0, yend = perc), color = "#012328") +
-  geom_label_repel(aes(item, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
+  geom_segment(aes(x = School, xend = School, y = 0, yend = perc), color = "#012328") +
+  geom_label_repel(aes(School, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
   scale_y_continuous(
     breaks = seq(0, 40, 10),
     limits = c(0, 40)
@@ -80,18 +77,31 @@ num_question <- 2
 
 data_cluster0_question2 <-
   cluster %>%
-  filter(question == questions[num_question]) %>%
-  droplevels()
+  filter(
+    Finished == TRUE & # only completed surveys
+      question == questions[num_question]
+  ) %>%
+  droplevels() %>% 
+  pivot_wider(
+    id_cols = participant,
+    names_from = question,
+    values_from = item
+  ) %>%
+  count(Position) %>% 
+  mutate(
+    perc = round(n / sum(n) * 100, 2),
+    lab_perc = paste(perc, "%", sep = "")
+  )
 
 lollipop_cluster0_question2 <-
   data_cluster0_question2 %>%
-  ggplot(aes(x = reorder(item, perc), y = perc)) +
+  ggplot(aes(x = reorder(Position, perc), y = perc)) +
   geom_point(size = 6, color = "#0C8066") +
-  geom_segment(aes(x = item, xend = item, y = 0, yend = perc), color = "#012328") +
-  geom_label_repel(aes(item, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
+  geom_segment(aes(x = Position, xend = Position, y = 0, yend = perc), color = "#012328") +
+  geom_label_repel(aes(Position, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
   scale_y_continuous(
-    breaks = seq(0, 50, 10),
-    limits = c(0, 50)
+    breaks = seq(0, 40, 10),
+    limits = c(0, 40)
   ) +
   labs(
     title = "Position",
@@ -104,19 +114,16 @@ lollipop_cluster0_question2
 
 # Merge in one figure ----------------------------------------------------------------
 
-lollipop_figure1 <-
+lollipop_figure01 <-
   lollipop_cluster0_question1 / lollipop_cluster0_question2 +
-  plot_annotation(
-    title = "Respondent Characteristics"
-  ) &
   theme(plot.title = element_text(size = 26, hjust = .5))
 
-lollipop_figure1
+lollipop_figure01
 
 # save to file
 ggsave(
-  filename = "figure1.png",
-  plot = lollipop_figure1,
+  filename = "figure01.png",
+  plot = lollipop_figure01,
   device = "png",
   path = here("img"),
   scale = 3,
