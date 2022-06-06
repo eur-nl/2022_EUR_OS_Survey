93830163227c57783f6f677fa48c7df11f4f1bea
Antonio Schettino <schettino@eur.nl>
2022-06-06 18:36:47 +0200

    new preproc cluster1

--- code/02_preprocessing_cluster1.R
+++ code/02_preprocessing_cluster1.R
@@ -14,33 +14,16 @@ set.seed(seed_proj)
 library(here)
 library(tidyverse)
 
-# load custom function with recoding scheme
-source(here("code", "functions", "recoding.R"))
-
 # Data --------------------------------------------------------
 
-OS_data_clean <-
-  readRDS(here("data", "preprocessed", "rds", "CLEAN_OS_Survey_responses.rds"))
-
 cluster1 <-
-  OS_data_clean %>%
+  readRDS(here("data", "preprocessed", "rds", "CLEAN_OS_Survey_responses.rds")) %>% 
   filter(
     Finished == TRUE & # only completed surveys
-    cluster %in% c(0, 1) & # keep questions of relevant cluster + cluster0
-    question != "Department" # omit department info
+    cluster == 1 # keep questions of relevant cluster
   ) %>%
-  droplevels() %>% # drop unused levels
-  select_if(~ sum(!is.na(.)) > 0) %>% # keep columns without NAs
-  pivot_longer( # convert to long format
-    "value_1":tail(names(.), 1),
-    names_to = "value",
-    values_to = "item"
-  ) %>%
-  filter(!is.na(item)) %>%
-  mutate(
-    item = gsub("Other_", "", item), # delete "Other_" from free text responses
-    item = fct_recode(item, !!!recode_all) # recode items for better readability
-  )
+  select(-c(Finished, Department, cluster)) %>% # omit irrelevant columns
+  count(School, Position, question, response)
 
 # save as .rds (to keep formatting in R)
 saveRDS(
