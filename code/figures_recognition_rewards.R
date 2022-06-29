05418e4ff1dd8bb83aef9ffac73d9c13420e399a
Antonio Schettino <schettino@eur.nl>
2022-06-29 09:09:33 +0200

    figures recognition & rewards

--- code/figures_recognition_rewards.R
+++ code/figures_recognition_rewards.R
@@ -82,18 +82,10 @@ EUR_OS_recognition_rewards_Q3 <-
     lab_perc = paste(perc, "%", sep = "")
   )
 
-# FROM HERE ----------------------------------------------------------------
-
-
-
-
-
-
-
 # Question 1, lollipop graph ----------------------------------------------------------------
 
 # EUR
-lollipop_cluster6_question1 <-
+lollipop_cluster7_question1 <-
   EUR_OS_recognition_rewards_Q1 %>%
   ggplot(aes(x = fct_rev(response), y = perc)) +
   geom_point(size = 6, color = "#0C8066") +
@@ -105,20 +97,20 @@ lollipop_cluster6_question1 <-
   ) +
   scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
   labs(
-    title = "Expected EUR recognition_rewards - EUR",
+    title = "Feeling recognized for Open Science - EUR",
     x = ""
   ) +
   coord_flip() +
   theme_custom
 
-lollipop_cluster6_question1
+lollipop_cluster7_question1
 
 # save to file
 ggsave(
-  filename = "figure_EUR_recognition_rewards_EUR.png",
-  plot = lollipop_cluster6_question1,
+  filename = "figure_feeling_recognized_EUR.png",
+  plot = lollipop_cluster7_question1,
   device = "png",
-  path = here("img", "recognition_rewards", "EUR"),
+  path = here("img", "recognition_rewards", "feeling_recognized"),
   scale = 3,
   width = 8,
   height = 8,
@@ -133,12 +125,12 @@ for(i in levels(EUR_OS_recognition_rewards$School)) {
   temp_figure_school <- 
     EUR_OS_recognition_rewards %>% 
     filter(
-      question == "Do you expect EUR to recognition_rewards you in learning open science practices?" &
+      question == "Do you feel recognized and rewarded by EUR (e.g. in the R&O cycle or appraisal conversation) for the Open Science activities you undertake?" &
       School == i
       ) %>% 
     count(question, response) %>%
     mutate(
-      response = fct_relevel(response, !!!Likert_EUR_recognition_rewards_convert),
+      response = fct_relevel(response, !!!Likert_feeling_recognized_convert),
       perc = round(n / sum(n) * 100, 2),
       lab_perc = paste(perc, "%", sep = "")
     ) %>% 
@@ -147,12 +139,12 @@ for(i in levels(EUR_OS_recognition_rewards$School)) {
     geom_segment(aes(x = response, xend = response, y = 0, yend = perc), color = "#012328") +
     geom_label_repel(aes(response, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
     scale_y_continuous(
-      breaks = seq(0, 90, 10),
-      limits = c(0, 90)
+      breaks = seq(0, 60, 10),
+      limits = c(0, 60)
     ) +
     scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
     labs(
-      title = paste0("Expected EUR recognition_rewards - ", i),
+      title = paste0("Feeling recognized for Open Science - ", i),
       x = ""
     ) +
     coord_flip() +
@@ -160,10 +152,10 @@ for(i in levels(EUR_OS_recognition_rewards$School)) {
   
   # save to file
   ggsave(
-    filename = paste0("figure_EUR_recognition_rewards_", i, ".png"),
+    filename = paste0("figure_feeling_recognized_", i, ".png"),
     plot = temp_figure_school,
     device = "png",
-    path = here("img", "recognition_rewards", "EUR"),
+    path = here("img", "recognition_rewards", "feeling_recognized"),
     scale = 3,
     width = 8,
     height = 8,
@@ -176,32 +168,32 @@ for(i in levels(EUR_OS_recognition_rewards$School)) {
 # Question 2, lollipop graph ----------------------------------------------------------------
 
 # EUR
-lollipop_cluster6_question2 <-
+lollipop_cluster7_question2 <-
   EUR_OS_recognition_rewards_Q2 %>%
   ggplot(aes(x = reorder(response, perc), y = perc)) +
   geom_point(size = 6, color = "#0C8066") +
   geom_segment(aes(x = response, xend = response, y = 0, yend = perc), color = "#012328") +
   geom_label_repel(aes(response, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
   scale_y_continuous(
-    breaks = seq(0, 50, 10),
-    limits = c(0, 50)
+    breaks = seq(0, 60, 10),
+    limits = c(0, 60)
   ) +
   scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
   labs(
-    title = "Desired Open Science recognition_rewards - EUR",
+    title = "Current recognition for Open Science - EUR",
     x = ""
   ) +
   coord_flip() +
   theme_custom
 
-lollipop_cluster6_question2
+lollipop_cluster7_question2
 
 # save to file
 ggsave(
-  filename = "figure_desired_recognition_rewards_EUR.png",
-  plot = lollipop_cluster6_question2,
+  filename = "figure_current_recognition_EUR.png",
+  plot = lollipop_cluster7_question2,
   device = "png",
-  path = here("img", "recognition_rewards", "desired"),
+  path = here("img", "recognition_rewards", "current_recognition"),
   scale = 3,
   width = 8,
   height = 8,
@@ -216,10 +208,95 @@ for(i in levels(EUR_OS_recognition_rewards$School)) {
   temp_figure_school <- 
     EUR_OS_recognition_rewards %>% 
     filter(
-      question == "Which of the following open science practices would you like EUR to provide information or recognition_rewards for?" &
+      question == "In what way were you recognized and rewarded?" &
         School == i
     ) %>% 
-    mutate(response = replace_na(response, "I don’t know/prefer not to answer")) %>% 
+    mutate(response = replace_na(response, "I don’t know/prefer not to answer")) %>%
+    mutate(response = fct_recode(response, !!!Likert_current_recognition_convert)) %>%
+    count(question, response) %>%
+    mutate(
+      perc = round(n / sum(n) * 100, 2),
+      lab_perc = paste(perc, "%", sep = "")
+    ) %>% 
+    ggplot(aes(x = reorder(response, perc), y = perc)) +
+    geom_point(size = 6, color = "#0C8066") +
+    geom_segment(aes(x = response, xend = response, y = 0, yend = perc), color = "#012328") +
+    geom_label_repel(aes(response, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
+    scale_y_continuous(
+      breaks = seq(0, 90, 10),
+      limits = c(0, 90)
+    ) +
+    scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
+    labs(
+      title = paste0("Current recognition for Open Science - ", i),
+      x = ""
+    ) +
+    coord_flip() +
+    theme_custom
+  
+  # save to file
+  ggsave(
+    filename = paste0("figure_current_recognition_rewards_", i, ".png"),
+    plot = temp_figure_school,
+    device = "png",
+    path = here("img", "recognition_rewards", "current_recognition"),
+    scale = 3,
+    width = 8,
+    height = 8,
+    units = "cm",
+    dpi = 600
+  )
+  
+}
+
+# Question 3, lollipop graph ----------------------------------------------------------------
+
+# EUR
+lollipop_cluster7_question3 <-
+  EUR_OS_recognition_rewards_Q3 %>%
+  ggplot(aes(x = reorder(response, perc), y = perc)) +
+  geom_point(size = 6, color = "#0C8066") +
+  geom_segment(aes(x = response, xend = response, y = 0, yend = perc), color = "#012328") +
+  geom_label_repel(aes(response, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
+  scale_y_continuous(
+    breaks = seq(0, 50, 10),
+    limits = c(0, 50)
+  ) +
+  scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
+  labs(
+    title = "Expected recognition for Open Science - EUR",
+    x = ""
+  ) +
+  coord_flip() +
+  theme_custom
+
+lollipop_cluster7_question3
+
+# save to file
+ggsave(
+  filename = "figure_expected_recognition_EUR.png",
+  plot = lollipop_cluster7_question3,
+  device = "png",
+  path = here("img", "recognition_rewards", "expected_recognition"),
+  scale = 3,
+  width = 8,
+  height = 8,
+  units = "cm",
+  dpi = 600
+)
+
+# separate graph for each school
+# NOTE: "Other" will not be reported
+for(i in levels(EUR_OS_recognition_rewards$School)) {
+  
+  temp_figure_school <- 
+    EUR_OS_recognition_rewards %>% 
+    filter(
+      question == "In what way do you expect to be recognized and rewarded?" &
+        School == i
+    ) %>% 
+    mutate(response = replace_na(response, "I don’t know/prefer not to answer")) %>%
+    mutate(response = fct_recode(response, !!!Likert_expected_recognition_convert)) %>%
     count(question, response) %>%
     mutate(
       perc = round(n / sum(n) * 100, 2),
@@ -235,101 +312,19 @@ for(i in levels(EUR_OS_recognition_rewards$School)) {
     ) +
     scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
     labs(
-      title = paste0("Desired Open Science recognition_rewards - ", i),
+      title = paste0("Expected recognition for Open Science - ", i),
       x = ""
     ) +
     coord_flip() +
-    theme_custom
+    theme_custom +
+    theme(plot.title = element_text(size = 20, hjust = .5))
   
   # save to file
   ggsave(
-    filename = paste0("figure_desired_recognition_rewards_", i, ".png"),
+    filename = paste0("figure_expected_recognition_", i, ".png"),
     plot = temp_figure_school,
     device = "png",
-    path = here("img", "recognition_rewards", "desired"),
-    scale = 3,
-    width = 8,
-    height = 8,
-    units = "cm",
-    dpi = 600
-  )
-  
-}
-
-# Question 3, lollipop graph ----------------------------------------------------------------
-
-# EUR
-lollipop_cluster6_question3 <-
-  EUR_OS_recognition_rewards_Q3 %>%
-  ggplot(aes(x = reorder(response, perc), y = perc)) +
-  geom_point(size = 6, color = "#0C8066") +
-  geom_segment(aes(x = response, xend = response, y = 0, yend = perc), color = "#012328") +
-  geom_label_repel(aes(response, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
-  scale_y_continuous(
-    breaks = seq(0, 60, 10),
-    limits = c(0, 60)
-  ) +
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
-  labs(
-    title = "FAIR recognition_rewards - EUR",
-    x = ""
-  ) +
-  coord_flip() +
-  theme_custom
-
-lollipop_cluster6_question3
-
-# save to file
-ggsave(
-  filename = "figure_FAIR_recognition_rewards_EUR.png",
-  plot = lollipop_cluster6_question3,
-  device = "png",
-  path = here("img", "recognition_rewards", "FAIR"),
-  scale = 3,
-  width = 8,
-  height = 8,
-  units = "cm",
-  dpi = 600
-)
-
-# separate graph for each school
-# NOTE: "Other" will not be reported
-for(i in levels(EUR_OS_recognition_rewards$School)) {
-  
-  temp_figure_school <- 
-    EUR_OS_recognition_rewards %>% 
-    filter(
-      question == "What recognition_rewards services provided at EUR have you used to make your data FAIR?" &
-        School == i
-    ) %>% 
-    mutate(response = replace_na(response, "I don't know/prefer not to answer")) %>% 
-    count(question, response) %>%
-    mutate(
-      perc = round(n / sum(n) * 100, 2),
-      lab_perc = paste(perc, "%", sep = "")
-    ) %>% 
-    ggplot(aes(x = reorder(response, perc), y = perc)) +
-    geom_point(size = 6, color = "#0C8066") +
-    geom_segment(aes(x = response, xend = response, y = 0, yend = perc), color = "#012328") +
-    geom_label_repel(aes(response, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
-    scale_y_continuous(
-      breaks = seq(0, 100, 10),
-      limits = c(0, 100)
-    ) +
-    scale_x_discrete(labels = function(x) str_wrap(x, width = 40)) +
-    labs(
-      title = paste0("FAIR recognition_rewards - ", i),
-      x = ""
-    ) +
-    coord_flip() +
-    theme_custom
-  
-  # save to file
-  ggsave(
-    filename = paste0("figure_FAIR_recognition_rewards_", i, ".png"),
-    plot = temp_figure_school,
-    device = "png",
-    path = here("img", "recognition_rewards", "FAIR"),
+    path = here("img", "recognition_rewards", "expected_recognition"),
     scale = 3,
     width = 8,
     height = 8,
