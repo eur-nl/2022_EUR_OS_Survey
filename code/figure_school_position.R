0ea19697cdb8f43cbf17acf45f6246bd9202cf42
Antonio Schettino <schettino@eur.nl>
2022-06-30 16:12:21 +0200

    regenerate figure school & position

--- code/figure_school_position.R
+++ code/figure_school_position.R
@@ -18,7 +18,7 @@ library(tidyverse)
 library(ggrepel)
 library(patchwork)
 
-source(here("code", "functions", "theme_custom.R")) # custom ggplot2 theme
+source(here("code", "functions", "lolliplot.R")) # custom ggplot2 plot
 
 # Data ----------------------------------------------------------------
 
@@ -29,67 +29,50 @@ cluster <-
 
 # School, lollipop graph ----------------------------------------------------------------
 
-data_cluster0_question1 <-
+lollipop_cluster0_question1 <-
   cluster %>%
   count(School) %>% 
   mutate(
     perc = round(n / sum(n) * 100, 2),
     lab_perc = paste(perc, "%", sep = "")
-  )
-
-lollipop_cluster0_question1 <-
-  data_cluster0_question1 %>%
-  ggplot(aes(x = reorder(School, perc), y = perc)) +
-  geom_point(size = 6, color = "#0C8066") +
-  geom_segment(aes(x = School, xend = School, y = 0, yend = perc), color = "#012328") +
-  geom_label_repel(aes(School, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
-  scale_y_continuous(
-    breaks = seq(0, 40, 10),
-    limits = c(0, 40)
-  ) +
-  labs(
+  ) %>%
+  lolliplot(
+    data = ., 
+    x = reorder(School, perc),
+    y = perc, 
+    labels = lab_perc, 
+    y_max_limit = 40,
     title = "School",
-    x = ""
-  ) +
-  coord_flip() +
-  theme_custom
+    title_size = 22
+  )
 
 lollipop_cluster0_question1
 
 # Position, lollipop graph ----------------------------------------------------------------
 
-data_cluster0_question2 <-
+lollipop_cluster0_question2 <-
   cluster %>%
   count(Position) %>% 
   mutate(
     perc = round(n / sum(n) * 100, 2),
     lab_perc = paste(perc, "%", sep = "")
-  )
-
-lollipop_cluster0_question2 <-
-  data_cluster0_question2 %>%
-  ggplot(aes(x = reorder(Position, perc), y = perc)) +
-  geom_point(size = 6, color = "#0C8066") +
-  geom_segment(aes(x = Position, xend = Position, y = 0, yend = perc), color = "#012328") +
-  geom_label_repel(aes(Position, perc, label = lab_perc), size = 4, nudge_y = 4, segment.alpha = 0, fill = "white", color = "#171C54") +
-  scale_y_continuous(
-    breaks = seq(0, 40, 10),
-    limits = c(0, 40)
-  ) +
-  labs(
+  ) %>%
+  lolliplot(
+    data = ., 
+    x = reorder(Position, perc),
+    y = perc, 
+    labels = lab_perc, 
+    y_max_limit = 40,
     title = "Position",
-    x = ""
-  ) +
-  coord_flip() +
-  theme_custom
+    title_size = 22
+  )
 
 lollipop_cluster0_question2
 
 # Merge in one figure ----------------------------------------------------------------
 
 lollipop_figure_school_position <-
-  lollipop_cluster0_question1 / lollipop_cluster0_question2 +
-  theme(plot.title = element_text(size = 26, hjust = .5))
+  lollipop_cluster0_question1 / lollipop_cluster0_question2 
 
 lollipop_figure_school_position
 
